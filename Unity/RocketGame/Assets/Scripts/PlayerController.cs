0189f8c774c9fc5305ed6c09efbfe1905990c3ec
greengiant83 <greengiant83@hotmail.com>
2022-07-27 16:07:01 -0500

    Added jumping

---
+++ Unity/RocketGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using Autohand;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Hand RightHand;
+    [SerializeField] private Hand LeftHand;
+    [SerializeField] private Rigidbody Body;
+    [SerializeField] private Transform Head;
+    [SerializeField] private float JumpSpeed = 1;
+    [SerializeField] private AutoHandPlayer Player;
+
+    void Start()
+    {
+        RightHand.OnSqueezed += RightHand_OnSqueezed; //Trigger button
+        RightHand.OnTriggerGrab += RightHand_OnTriggerGrab; //Grip button
+    }
+
+    private void OnDestroy()
+    {
+        RightHand.OnSqueezed -= RightHand_OnSqueezed; //Trigger button
+        RightHand.OnTriggerGrab -= RightHand_OnTriggerGrab; //Grip button
+    }
+
+    private void jump()
+    {
+        Debug.Log("Jump");
+        Player.body.AddForce(Vector3.up * JumpSpeed);
+        Debug.Log(Player.body == Body);
+    }
+
+    private void RightHand_OnTriggerGrab(Hand hand, Grabbable grabbable) //Execute when Grip button is pressed
+    {
+        jump();
+    }
+
+    private void RightHand_OnSqueezed(Hand hand, Grabbable grabbable) //Executed when Trigger button is pressed
+    {
+        
+    }
+}
